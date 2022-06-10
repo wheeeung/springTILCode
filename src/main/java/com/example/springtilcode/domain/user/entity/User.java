666b924e966bcf5f78e1f08390bb52f4ebc916fe
wheeeung <whee050916@gmail.com>
2022-06-10 16:22:10 +0900

    error solving

--- src/main/java/com/example/springtilcode/domain/user/entity/User.java
+++ src/main/java/com/example/springtilcode/domain/user/entity/User.java
@@ -12,6 +12,7 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
--- src/main/java/com/example/springtilcode/global/config/SecurityConfig.java
+++ src/main/java/com/example/springtilcode/global/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,12 +17,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    @Bean
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
                 .headers()
                 .frameOptions().sameOrigin()
