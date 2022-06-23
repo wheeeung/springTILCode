540fcbdf11d8bb4bf9ba4b8b85ceea351fff0af0
wheeeung <whee050916@gmail.com>
2022-06-23 10:26:11 +0900

    add repository query dsl

--- src/main/java/com/example/springtilcode/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/springtilcode/domain/user/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.example.springtilcode.domain.user.repository;
 
 import com.example.springtilcode.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    @Query("select User.name, User.email from User where User.id in (select User.id from User where User.role = 0)")
+    List<User> findByUser();
 }
