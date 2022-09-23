201cc53a68ae1461de1ce0e257f1dcd43a567eaa
wheeeung <whee050916@gmail.com>
2022-09-23 16:14:15 +0900

    delete file

--- src/main/java/com/example/springtilcode/domain/board/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springtilcode.domain.board.repository;
-
-import com.example.springtilcode.domain.board.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
