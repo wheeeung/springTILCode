16d8c979b812351311621cb88a92a3192a02df3b
wheeeung <whee050916@gmail.com>
2022-12-24 20:31:51 +0900

    add entity kt

---
+++ kotlin/src/main/kotlin/com/example/demo/domain/feed/entity/Feed.kt
@@ -0,0 +1,20 @@
+package com.example.demo.domain.feed.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Feed {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long?= null
+
+    @Column
+    var title: String?= null
+
+    @Column
+    var content: String?= null
+}
