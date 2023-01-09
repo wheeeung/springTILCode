f8b2b193c1c33002e5da6d377ac244ee55d3230c
wheeeung <whee050916@gmail.com>
2023-01-09 12:07:39 +0900

    edit feed service

--- kotlin/src/main/kotlin/com/example/demo/domain/feed/controller/dto/response/FeedResponse.kt
+++ kotlin/src/main/kotlin/com/example/demo/domain/feed/controller/dto/response/FeedResponse.kt
@@ -1,13 +1,16 @@
 package com.example.demo.domain.feed.controller.dto.response
 
 import com.example.demo.domain.feed.entity.Feed
+import com.example.demo.domain.user.entity.User
 
 data class FeedResponse(
     val title:String?,
-    val content: String?
+    val content: String?,
+    val user: User?
 ){
     constructor(feed: Feed): this(
         title = feed.title,
-        content = feed.content
+        content = feed.content,
+        user = feed.user
     )
 }
--- kotlin/src/main/kotlin/com/example/demo/domain/feed/service/FeedService.kt
+++ kotlin/src/main/kotlin/com/example/demo/domain/feed/service/FeedService.kt
@@ -5,20 +5,28 @@ import com.example.demo.domain.feed.controller.dto.response.FeedResponse
 import com.example.demo.domain.feed.entity.Feed
 import com.example.demo.domain.feed.exception.FeedNotFoundException
 import com.example.demo.domain.feed.repository.FeedRepository
+import com.example.demo.domain.user.entity.User
+import com.example.demo.domain.user.exception.EmailNotFoundException
+import com.example.demo.domain.user.repository.UserRepository
 import com.example.demo.global.error.GlobalErrorCode
+import com.example.demo.global.util.SecurityUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class FeedService (
-    private val feedRepository: FeedRepository
+    private val feedRepository: FeedRepository,
+    private val userRepository: UserRepository,
+    private val securityUtil: SecurityUtil
 ){
     @Transactional
     fun create(request: FeedRequest): FeedResponse{
+        val user: User = userRepository.findByEmail(securityUtil.getEmail()) ?: throw EmailNotFoundException(GlobalErrorCode.EMAIL_NOT_FOUND)
         val feed: Feed = feedRepository.save(
             Feed(
                title = request.title,
-               content = request.content
+               content = request.content,
+               user = user
             )
         )
         return FeedResponse(feed)
