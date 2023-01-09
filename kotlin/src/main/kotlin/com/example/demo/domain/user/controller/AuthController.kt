d716e402aa0e4d92676eac0963bb0ca4e9217ef7
wheeeung <whee050916@gmail.com>
2023-01-09 19:25:59 +0900

    add valid in user

--- kotlin/src/main/kotlin/com/example/demo/domain/user/controller/AuthController.kt
+++ kotlin/src/main/kotlin/com/example/demo/domain/user/controller/AuthController.kt
@@ -3,6 +3,7 @@ package com.example.demo.domain.user.controller
 import com.example.demo.domain.user.controller.dto.request.UserRequest
 import com.example.demo.domain.user.controller.dto.response.TokenResponse
 import com.example.demo.domain.user.service.AuthService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,12 +13,12 @@ class AuthController (
     private val authService: AuthService
 ){
     @PostMapping("/signup")
-    fun signup(@RequestBody userRequest: UserRequest){
+    fun signup(@Valid @RequestBody userRequest: UserRequest){
         authService.signup(userRequest.email, userRequest.password)
     }
 
     @PostMapping("/login")
-    fun login(@RequestBody userRequest: UserRequest): TokenResponse{
+    fun login(@Valid @RequestBody userRequest: UserRequest): TokenResponse{
         return authService.login(userRequest.email, userRequest.password)
     }
 }
--- kotlin/src/main/kotlin/com/example/demo/domain/user/controller/dto/request/UserRequest.kt
+++ kotlin/src/main/kotlin/com/example/demo/domain/user/controller/dto/request/UserRequest.kt
@@ -1,6 +1,13 @@
 package com.example.demo.domain.user.controller.dto.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+
 data class UserRequest(
+    @NotBlank
+    @Email
     val email: String,
+
+    @NotBlank
     val password: String
 )
