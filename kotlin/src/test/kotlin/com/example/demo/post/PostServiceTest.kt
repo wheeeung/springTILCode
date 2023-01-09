bb0df54c015e2891b43aaba058531fefcc1837d5
wheeeung <whee050916@gmail.com>
2023-01-09 20:25:56 +0900

    add testcode

---
+++ kotlin/src/test/kotlin/com/example/demo/post/PostServiceTest.kt
@@ -0,0 +1,29 @@
+package com.example.demo.post
+
+import com.example.demo.domain.feed.controller.dto.request.PostRequest
+import com.example.demo.domain.feed.service.PostService
+import com.example.demo.global.security.UserAuthentication
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.core.context.SecurityContextHolder
+
+@SpringBootTest
+class PostServiceTest (
+    @Autowired val postService: PostService
+){
+    @BeforeEach
+    fun setUser(){
+        val userAuthentication = UserAuthentication("test@gmail.com", null, null)
+        SecurityContextHolder.getContext().authentication = userAuthentication
+    }
+
+    @Test
+    @DisplayName("1. 글 저장")
+    fun save(){
+        val request = PostRequest("title", "content")
+        postService.create(request)
+    }
+}
