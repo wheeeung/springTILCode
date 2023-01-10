4695a436ee2f1010668ca051070f90273fa43415
wheeeung <whee050916@gmail.com>
2023-01-10 15:12:30 +0900

    add entity

--- SpringTILCode/src/main/java/com/example/springtilcode/domain/post/entity/Post.java
+++ SpringTILCode/src/main/java/com/example/springtilcode/domain/post/entity/Post.java
@@ -1,4 +1,25 @@
 package com.example.springtilcode.domain.post.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
 }
