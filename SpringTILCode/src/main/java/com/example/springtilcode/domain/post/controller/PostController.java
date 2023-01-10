db6d0e5f8d801cd6bdaac0893893812554661811
wheeeung <whee050916@gmail.com>
2023-01-10 16:14:14 +0900

    add controller

---
+++ SpringTILCode/src/main/java/com/example/springtilcode/domain/post/controller/PostController.java
@@ -0,0 +1,22 @@
+package com.example.springtilcode.domain.post.controller;
+
+import com.example.springtilcode.domain.post.controller.dto.request.PostRequest;
+import com.example.springtilcode.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping("/post")
+    public void save(@RequestBody PostRequest request){
+        postService.save(request.getTitle(), request.getContent());
+    }
+
+    @DeleteMapping("/post/{id}")
+    public void delete(@PathVariable Integer id){
+        postService.delete(id);
+    }
+}
