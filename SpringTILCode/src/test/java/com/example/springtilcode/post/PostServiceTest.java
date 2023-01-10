4431d993650f8b519570aa1d53066371f3775d96
wheeeung <whee050916@gmail.com>
2023-01-10 15:41:51 +0900

    add test code

---
+++ SpringTILCode/src/test/java/com/example/springtilcode/post/PostServiceTest.java
@@ -0,0 +1,19 @@
+package com.example.springtilcode.post;
+
+import com.example.springtilcode.domain.post.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostServiceTest {
+    @Autowired
+    private PostService postService;
+
+    @Test
+    @DisplayName("1. 글 쓰기")
+    public void save(){
+        postService.save("title", "content");
+    }
+}
