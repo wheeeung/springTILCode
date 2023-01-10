5f4cb64f981627c3ad980398c9297810a1fe5d81
wheeeung <whee050916@gmail.com>
2023-01-11 07:45:22 +0900

    add test

--- SpringTILCode/src/test/java/com/example/springtilcode/post/PostServiceTest.java
+++ SpringTILCode/src/test/java/com/example/springtilcode/post/PostServiceTest.java
@@ -1,15 +1,24 @@
 package com.example.springtilcode.post;
 
 import com.example.springtilcode.domain.post.service.PostService;
+import com.example.springtilcode.global.jwt.UserAuthentication;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootTest
 public class PostServiceTest {
     @Autowired
     private PostService postService;
+
+    @BeforeEach
+    public void setUser(){
+        UserAuthentication userAuthentication = new UserAuthentication("whee050916@gmail.com", null, null);
+        SecurityContextHolder.getContext().setAuthentication(userAuthentication);
+    }
 
     @Test
     @DisplayName("1. 글 쓰기")
