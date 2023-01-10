b00c2bad691cc36d569c90a5eff2e783d0c4fe94
wheeeung <whee050916@gmail.com>
2023-01-10 11:02:28 +0900

    add auth test code

---
+++ kotlin/src/test/kotlin/com/example/demo/user/AuthServiceTest.kt
@@ -0,0 +1,20 @@
+package com.example.demo.user
+
+import com.example.demo.domain.user.service.AuthService
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthServiceTest (
+    @Autowired
+    val authService: AuthService
+){
+    @Test
+    @DisplayName("1. 회원가입")
+    fun signup(){
+        authService.signup("whee@gmail.com", "1234")
+    }
+
+}
