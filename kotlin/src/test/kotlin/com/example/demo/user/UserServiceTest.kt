95b87bccd6525598680740810c0a4f92613700c9
wheeeung <whee050916@gmail.com>
2023-01-11 11:18:11 +0900

    add user service test

---
+++ kotlin/src/test/kotlin/com/example/demo/user/UserServiceTest.kt
@@ -0,0 +1,26 @@
+package com.example.demo.user
+
+import com.example.demo.domain.user.service.UserService
+import com.example.demo.global.security.UserAuthentication
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.core.context.SecurityContextHolder
+
+@SpringBootTest
+class UserServiceTest (
+    @Autowired val userService: UserService
+){
+    @BeforeEach
+    fun setUser(){
+        val userAuthentication = UserAuthentication("whee@gmail.com", null, null)
+        SecurityContextHolder.getContext().authentication = userAuthentication
+    }
+
+    @Test
+    fun getUser(){
+        userService.getUser()
+
+    }
+}
