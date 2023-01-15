76cf870a7f647bb22931228234b7cafdf953c669
wheeeung <whee050916@gmail.com>
2023-01-15 22:52:34 +0900

    add edit profile controller test

--- kotlin/src/test/kotlin/com/example/demo/user/controller/UserControllerTest.kt
+++ kotlin/src/test/kotlin/com/example/demo/user/controller/UserControllerTest.kt
@@ -1,10 +1,13 @@
 package com.example.demo.user.controller
 
+import com.example.demo.domain.user.controller.dto.request.EditProfileRequest
 import com.example.demo.domain.user.service.UserService
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -22,6 +25,21 @@ class UserControllerTest (
         mockMvc.perform(
             MockMvcRequestBuilders.get("/user")
                 .header("Authorization", "bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3aGVlQGdtYWlsLmNvbSIsImV4cCI6MTY3MzY2MTk2OX0.PxM1iNRKBl3fqcum56V7Xiju3PnhNTTmxYbgD6-12sY")
+        )
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun editProfile(){
+        val request = EditProfileRequest("whee1234@gmail.com")
+        val json = jacksonObjectMapper().writeValueAsString(request)
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.patch("/user")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .header("Authorization", "bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3aGVlQGdtYWlsLmNvbSIsImV4cCI6MTY3Mzc5NDI1NH0.dgqn2Y4jtNJvoXBYxeFeDV6Ujsll4cN6GPUR_pgd_5o")
         )
             .andExpect(status().isOk)
     }
