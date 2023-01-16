3c9839f46a6dc7e7c4814e2e8a37a00e6e088277
wheeeung <whee050916@gmail.com>
2023-01-16 12:47:39 +0900

    edit delete test

--- SpringTILCode/src/test/java/com/example/springtilcode/post/service/PostServiceTest.java
+++ SpringTILCode/src/test/java/com/example/springtilcode/post/service/PostServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.springtilcode.post.service;
 
 import com.example.springtilcode.domain.post.controller.dto.response.PostResponse;
+import com.example.springtilcode.domain.post.repository.PostRepository;
 import com.example.springtilcode.domain.post.service.PostService;
 import com.example.springtilcode.global.jwt.UserAuthentication;
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +16,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 public class PostServiceTest {
     @Autowired
     private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @BeforeEach
     public void setUser(){
@@ -40,6 +44,8 @@ public class PostServiceTest {
         Integer id = 1;
 
         postService.delete(id);
+
+        Assertions.assertTrue(postRepository.findById(id).isEmpty());
     }
 
     @Test
