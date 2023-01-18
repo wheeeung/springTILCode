e840b1b96ea4e9602d6879749e8a02e7b744f321
wheeeung <whee050916@gmail.com>
2023-01-18 11:54:46 +0900

    delete unused code

--- SpringTILCode/src/main/java/com/example/springtilcode/domain/user/controller/AuthController.java
+++ SpringTILCode/src/main/java/com/example/springtilcode/domain/user/controller/AuthController.java
@@ -2,11 +2,8 @@ package com.example.springtilcode.domain.user.controller;
 
 import com.example.springtilcode.domain.user.controller.dto.AuthRequest;
 import com.example.springtilcode.domain.user.controller.dto.TokenResponse;
-import com.example.springtilcode.domain.user.controller.dto.UserResponse;
 import com.example.springtilcode.domain.user.service.AuthService;
-import com.example.springtilcode.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +14,6 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-    private final UserService userService;
 
     @PostMapping("/signup")
     public void signup(@Valid @RequestBody AuthRequest request){
