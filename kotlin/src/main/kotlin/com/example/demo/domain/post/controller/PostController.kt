e7ea83dd7b931d70f2b4a213cba728dbb4d4b228
wheeeung <whee050916@gmail.com>
2023-01-28 10:43:44 +0900

    add editPost controller

--- kotlin/src/main/kotlin/com/example/demo/domain/post/controller/PostController.kt
+++ kotlin/src/main/kotlin/com/example/demo/domain/post/controller/PostController.kt
@@ -6,6 +6,7 @@ import com.example.demo.domain.post.service.PostService
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -28,6 +29,11 @@ class PostController (
     @GetMapping("/post/{id}")
     fun getPost(@PathVariable id: Long): PostResponse{
         return postService.getPost(id)
+    }
+
+    @PatchMapping("/post/{id}")
+    fun editPost(@PathVariable id: Long, @Valid @RequestBody request: PostRequest): PostResponse{
+        return postService.editPost(request, id);
     }
 
 }
