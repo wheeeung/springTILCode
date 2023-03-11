3d9b338910e06d7518f36126563db6751404d208
wheeeung <whee050916@gmail.com>
2023-03-12 07:09:22 +0900

    feat:: edit user detail

--- kotlin/src/main/kotlin/com/example/demo/global/user/UserDetailsImpl.kt
+++ kotlin/src/main/kotlin/com/example/demo/global/user/UserDetailsImpl.kt
@@ -2,11 +2,11 @@ package com.example.demo.global.user
 
 import com.example.demo.domain.user.entity.User
 import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class UserDetailsImpl(val user: User) : UserDetails{
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = arrayListOf(SimpleGrantedAuthority(user.role?.name))
 
     override fun getPassword(): String? = user.password
 
